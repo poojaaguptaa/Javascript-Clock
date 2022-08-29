01d50f04e14ce07d50cbe894ad6f98de7f6df66f
poojagupta <poojaaguptaa3@gmail.com>
2022-08-29 17:58:37 +0530

    Javascript clock

---
+++ js/index.js
@@ -0,0 +1,23 @@
+  const secondArrow = document.querySelector('.second-arrow');
+  const minArrow = document.querySelector('.minute-arrow');
+  const hourArrow = document.querySelector('.hour-arrow');
+
+  function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondArrow.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minArrow.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourArrow.style.transform = `rotate(${hourDegrees}deg)`;
+  }
+
+  setInterval(setDate, 1000);
+
+  setDate();
